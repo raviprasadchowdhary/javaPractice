58e68777c8f928aa96d41d99b2ce522d3916fd33
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2024-10-20 18:46:38 +0530

    second commit

--- practice/src/hello/Hello.java
+++ practice/src/hello/Hello.java
@@ -3,6 +3,6 @@ package hello;
 public class Hello {
 	public static void main(String args[]) {
 		System.out.println("Hello this is **** here!");
-		System.out.println("First commit!");
+		System.out.println("Second commit!");
 	}
 }
