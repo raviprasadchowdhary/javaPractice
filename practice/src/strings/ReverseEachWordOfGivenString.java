649b4daec61ea1947c59156ed99acbe6961ace2e
raviprasadchowdhary <78257726+raviprasadchowdhary@users.noreply.github.com>
2024-10-26 16:34:30 +0530

    reverse each word of a given string

---
+++ practice/src/strings/ReverseEachWordOfGivenString.java
@@ -0,0 +1,28 @@
+package strings;
+
+import java.util.Scanner;
+
+public class ReverseEachWordOfGivenString {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter a string of which each word is to be reversed: ");
+		String input = scan.nextLine();
+
+		reverseEachWordOfGivenString(input);
+	}
+
+	public static void reverseEachWordOfGivenString(String input) {
+		String[] words = input.split(" ");
+		for (int i = 0; i < words.length; i++) {
+			String reversedWord = "";
+			for (int j = words[i].length() - 1; j >= 0; j--) {
+				reversedWord += words[i].charAt(j);
+			}
+			System.out.print(reversedWord);
+			if (i < words.length - 1) {
+				System.out.print(" ");
+			}
+		}
+	}
+}
